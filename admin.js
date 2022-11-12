ed18287904eada39c38e44fb9dd1d8334cb2ec77
wirra1 <111612550+wirra1@users.noreply.github.com>
2022-11-12 01:23:45 +0100

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,15 @@
+function validate()
+{
+    var password=document.getElementByid("username").Value;
+    var password=document.getElementByid("password").Value;
+    if(username=="kalle"&& password=="hej");
+{
+    alert("Login klar");
+    return false;
+}
+else
+{
+    alert("log");
+}
+
+}
